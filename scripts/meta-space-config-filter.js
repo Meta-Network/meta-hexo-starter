f709a088a39f0ba9267ec58ed96a82d084d7842e
Casimir Crystal <casimir.crystal.blue@outlook.com>
2021-10-26 19:25:00 +0800

    feat: add meta space config filter

---
+++ scripts/meta-space-config-filter.js
@@ -0,0 +1,27 @@
+const fs = require('hexo-fs');
+const { deepMerge } = require('hexo-util');
+const path = require('path');
+
+// eslint-disable-next-line no-undef
+hexo.extend.filter.register('after_init', async function () {
+  const { base_dir, render } = this;
+
+  const metaConfigPath = path.join(base_dir, 'meta-space-config.yml');
+  const isExists = await fs.exists(metaConfigPath);
+  if (!isExists) return;
+
+  const metaConfig = await render.render({ path: metaConfigPath });
+  const { user, site } = metaConfig;
+  const stunConfig = {};
+
+  if (site && user) {
+    stunConfig.author = {
+      avatar: {
+        url: site.avatar,
+      },
+      motto: site.description,
+    }
+  }
+
+  this.config.theme_config = deepMerge(this.config.theme_config, stunConfig);
+});
